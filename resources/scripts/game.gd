55406497c6c1d8fd5da216752286710ea7697227
estelaGamba <sparren.contato@gmail.com>
2026-06-12 19:45:05 -0300

    0.0.3
    - agora o jogo fecha ao terminar
    (feito isso apenas para o jogo ser testável)

--- resources/scripts/game.gd
+++ resources/scripts/game.gd
@@ -3,7 +3,15 @@ extends Node2D
 
 func _ready() -> void:
 	Dialogic.start("intro")
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+func _on_dialogic_signal(argument:String):
+	if argument == "cabou":
+		get_tree().quit()
+		#quando acabar rodar os creditos
+		#mas agora que ainda nao ta terminado so fechar o jogo
+		#jumpscare de fechar jogo
+
+func sair_do_jogo() -> void:
+	pass
