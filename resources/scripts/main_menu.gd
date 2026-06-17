22cd4d237e3af3ac53d3d06de11de30614044732
estelaGamba <sparren.contato@gmail.com>
2026-06-17 19:03:03 -0300

    0.0.6
    - iniciando o menu inicial

---
+++ resources/scripts/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MarginContainer/VBoxContainer/continue
+	pass
+
+func on_button_presed():
+	pass
